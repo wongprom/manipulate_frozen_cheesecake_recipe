5cf58eaa95837a059248d353d27c4fbe1cadfb19
wongprom <>
2024-07-08 15:41:07 +0200

    6. Change estimate time, move querySelectos to top.

--- wrong.js
+++ wrong.js
@@ -1,24 +1,28 @@
 console.log('test från wrong');
-//? 1. The logo text of the site has the wrong color. Change it to the correct one.
+
 const logoText = document.querySelector('.logo-text');
+const recipeName = document.querySelector('#recipe-name');
+const header = document.querySelector('header');
+const timeContainer = document.querySelector('.time-container');
+
+//? 1. The logo text of the site has the wrong color. Change it to the correct one.
 logoText.style.color = '#384241';
 
 // 2. The alignment of the elements inside the header element are wrong. Change it to the correct one. Hint, check the flex properties for the correct alignment. Here is a link that might help: https://css-tricks.com/snippets/css/a-guide-to-flexbox/
-const header = document.querySelector('header');
 header.style.justifyContent = 'unset';
 
 // 3. The header has a border at the bottom, but it has the wrong color. Change it do the correct one.
 header.style.borderBottom = '1px solid lightgray';
 
 // 4. The recipe name is wrong, change it to the correct one.
-const recipeName = document.querySelector('#recipe-name');
 recipeName.innerText = 'Frozen Cheescake';
 
 // 5. The clock icon beneath the recipe name has disappeared and been replaced by a text instead. This can be fixed by adding a class to that element.
-const timeContainer = document.querySelector('.time-container');
 timeContainer.firstElementChild.classList.add('material-icons');
 
 // 6. The estimated time of the recipe is also incorrect. Change it to the correct time estimation.
+timeContainer.lastElementChild.innerHTML = '60+ min';
+
 // 7. The src path to the image is wrong, or atleast it's showing the wrong image. Change it to the correct one. The available images can be found in the assets folder.
 // 8. The background color of the ingredients list container is wrong. Fix it.
 // 9. The ingredients are divided in to two parts, one for the bottom and one for the paste. In the list of the ingredients to the bottom, there is a text instead of two list items. Remove the text and add those two list items.
