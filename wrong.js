988eaefdedbb1d6946968b5f139a75d02dd3ab9d
wongprom <>
2024-07-08 15:15:24 +0200

    3. Change border-bottom color.

--- wrong.js
+++ wrong.js
@@ -5,10 +5,10 @@ logoText.style.color = '#384241';
 
 // 2. The alignment of the elements inside the header element are wrong. Change it to the correct one. Hint, check the flex properties for the correct alignment. Here is a link that might help: https://css-tricks.com/snippets/css/a-guide-to-flexbox/
 const header = document.querySelector('header');
-nav.style.justifyContent = 'unset';
+header.style.justifyContent = 'unset';
 
 // 3. The header has a border at the bottom, but it has the wrong color. Change it do the correct one.
-
+header.style.borderBottom = '1px solid lightgray';
 // const test = header.style.gap;
 // console.log('🚀 ~ test:', test);
 
