a30ce16afe7ed5ae8870d615c6873a120d9ba46e
wongprom <>
2024-07-08 15:21:00 +0200

    4. Change recipe name.

--- wrong.js
+++ wrong.js
@@ -9,10 +9,11 @@ header.style.justifyContent = 'unset';
 
 // 3. The header has a border at the bottom, but it has the wrong color. Change it do the correct one.
 header.style.borderBottom = '1px solid lightgray';
-// const test = header.style.gap;
-// console.log('🚀 ~ test:', test);
 
 // 4. The recipe name is wrong, change it to the correct one.
+const recipeName = document.querySelector('#recipe-name');
+recipeName.innerText = 'Frozen Cheescake';
+
 // 5. The clock icon beneath the recipe name has disappeared and been replaced by a text instead. This can be fixed by adding a class to that element.
 // 6. The estimated time of the recipe is also incorrect. Change it to the correct time estimation.
 // 7. The src path to the image is wrong, or atleast it's showing the wrong image. Change it to the correct one. The available images can be found in the assets folder.
